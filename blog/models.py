81da96518b6bb80c56977c918c590f8b79eeee04
Yiknok <danyapavl20.03@gmail.com>
2022-10-06 17:47:01 +0300

    add Menu templatetag

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -8,10 +9,13 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug': self.slug})
+
     class Meta:
         ordering = ['title']
-        verbose_name='Category'
-        verbose_name_plural='Categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class Tag(models.Model):
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns=[
-    path('',index,name='home'),
+urlpatterns = [
+    path('', index, name='home'),
+    path('category/<str:slug>', get_category, name='category'),
 ]
