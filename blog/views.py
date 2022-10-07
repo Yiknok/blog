8cecb4c516d4ee83fadb6b655778caff92753aff
Yiknok <danyapavl20.03@gmail.com>
2022-10-07 18:28:18 +0300

    view posts on Home page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+import blog.models as blog_models
+
+
+class Home(ListView):
+    model = blog_models.Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Yiknok`s backend'
+        return context
 
 
 def index(request):
@@ -6,4 +20,8 @@ def index(request):
 
 
 def get_category(request, slug):
+    return render(request, 'blog/category.html')
+
+
+def get_post(request, slug):
     return render(request, 'blog/category.html')
