c55be2a6422d5ce26522b69b3f7bd9b5c0269de2
Yiknok <danyapavl20.03@gmail.com>
2022-10-09 13:10:40 +0300

    view posts by category

--- blog/views.py
+++ blog/views.py
@@ -7,20 +7,32 @@ class Home(ListView):
     model = blog_models.Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Yiknok`s backend'
         return context
 
+# def index(request):
+#     return render(request, 'blog/index.html')
 
-def index(request):
-    return render(request, 'blog/index.html')
+class PostsByCategory(ListView):
+    template_name = 'blog/category.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    allow_empty = False
+
+    def get_queryset(self):
+        return blog_models.Post.objects.filter(category__slug=self.kwargs['slug'])
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = blog_models.Category.objects.get(slug=self.kwargs['slug'])
+        return context
 
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+# def get_category(request, slug):
+#     return render(request, 'blog/category.html')
 
 
 def get_post(request, slug):
