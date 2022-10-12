302e3ceb092cbc98571e46add5fc552d9529f016
Yiknok <danyapavl20.03@gmail.com>
2022-10-12 15:00:49 +0300

    single post page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 import blog.models as blog_models
+from django.db.models import F
 
 
 class Home(ListView):
@@ -14,6 +14,7 @@ class Home(ListView):
         context['title'] = 'Yiknok`s backend'
         return context
 
+
 # def index(request):
 #     return render(request, 'blog/index.html')
 
@@ -25,6 +26,7 @@ class PostsByCategory(ListView):
 
     def get_queryset(self):
         return blog_models.Post.objects.filter(category__slug=self.kwargs['slug'])
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = blog_models.Category.objects.get(slug=self.kwargs['slug'])
@@ -35,5 +37,18 @@ class PostsByCategory(ListView):
 #     return render(request, 'blog/category.html')
 
 
-def get_post(request, slug):
-    return render(request, 'blog/category.html')
+class GetPost(DetailView):
+    model = blog_models.Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
+
+
+class PostsByTag(ListView):
+    pass
