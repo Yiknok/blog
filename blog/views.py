29048bd637b0e01217f771ede9d733fe736bf4ef
Yiknok <danyapavl20.03@gmail.com>
2022-10-13 19:10:14 +0300

    Get posts be tag

--- blog/views.py
+++ blog/views.py
@@ -33,8 +33,19 @@ class PostsByCategory(ListView):
         return context
 
 
-# def get_category(request, slug):
-#     return render(request, 'blog/category.html')
+class PostsByTag(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    allow_empty = False
+
+    def get_queryset(self):
+        return blog_models.Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Posts by tag: ' + str(blog_models.Tag.objects.get(slug=self.kwargs['slug']))
+        return context
 
 
 class GetPost(DetailView):
@@ -48,7 +59,3 @@ class GetPost(DetailView):
         self.object.save()
         self.object.refresh_from_db()
         return context
-
-
-class PostsByTag(ListView):
-    pass
